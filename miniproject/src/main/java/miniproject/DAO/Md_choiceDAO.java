d078bd984373bd5d46fd565e401ad3435e316063
h0xxln <alxl47@naver.com>
2025-03-26 16:18:48 +0900

    Md_choice -> index 페이지 (추천 매물 정보) Md_choiceDAO 생성

---
+++ miniproject/src/main/java/miniproject/DAO/Md_choiceDAO.java
@@ -0,0 +1,24 @@
+package miniproject.DAO;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import miniproject.DTO.Md_choiceDTO;
+import miniproject.Mapper.Md_choice_mapper;
+
+@Repository("Md_choiceDAO")
+public class Md_choiceDAO implements Md_choice_mapper {
+	@Resource(name="template")
+	public SqlSessionTemplate md_st;
+	
+	@Override
+	public List<Md_choiceDTO> mdchoice_select(Md_choiceDTO mddto) {
+		List<Md_choiceDTO> md_choiceList = this.md_st.selectList("mdchoice_select");
+		
+		return md_choiceList;
+	}
+}
