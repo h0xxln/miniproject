0c5ba699ed01c391f60b4fae7abd65b83340a9d3
h0xxln <alxl47@naver.com>
2025-03-26 16:08:19 +0900

    Web_info(카피라이터) dto 생성

---
+++ miniproject/src/main/java/miniproject/DTO/Web_infoDTO.java
@@ -0,0 +1,11 @@
+package miniproject.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Web_infoDTO {
+	String w_cpname, w_ceo, w_adress, w_bidnum,  w_trade;
+	String w_iso, w_biq, w_pax;
+}
