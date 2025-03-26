709aa27b4eb435fec67605bc2c03f72a205243f5
h0xxln <alxl47@naver.com>
2025-03-26 16:11:39 +0900

    /최종/Web_info(카피라이터 데이터 관련) Mapper 생성

---
+++ miniproject/src/main/java/miniproject/Mapper/Web_info_mapper.java
@@ -0,0 +1,16 @@
+package miniproject.Mapper;
+
+import java.util.List;
+
+
+import miniproject.DTO.Md_choiceDTO;
+import miniproject.DTO.Sign_DTO;
+import miniproject.DTO.Web_infoDTO;
+
+
+//함수는 동사로 시작
+public interface Web_info_mapper { 
+
+	public List<Web_infoDTO> webInfo_select(Web_infoDTO webdto);
+	
+}
