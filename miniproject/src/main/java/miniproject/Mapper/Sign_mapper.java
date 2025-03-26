fa625d03af6df5df6a9010ace18b7d608baf80d0
h0xxln <alxl47@naver.com>
2025-03-26 16:58:44 +0900

    회원 관련 정보(회원가입, 로그인, 이메일 찾기, .. ) Sign_Mapper 생성

---
+++ miniproject/src/main/java/miniproject/Mapper/Sign_mapper.java
@@ -0,0 +1,18 @@
+package miniproject.Mapper;
+
+import java.util.List;
+
+
+import miniproject.DTO.Md_choiceDTO;
+import miniproject.DTO.Sign_DTO;
+import miniproject.DTO.Web_infoDTO;
+
+//회원 관련 mapper
+//함수는 동사로 시작
+public interface Sign_mapper {
+	List<Sign_DTO> login_select(Sign_DTO dto); //로그인 
+	public int delete_member(Sign_DTO dto); //회원 삭제
+	public int signup_insert(Sign_DTO dto);	//회원 가입 
+	Sign_DTO search_email(String m_name, String m_number);
+	public int check_email(String m_email);
+}
