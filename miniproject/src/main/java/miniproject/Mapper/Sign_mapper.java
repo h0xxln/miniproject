6e004ba2762d0bfe888663c714a8dc1236c4126d
h0xxln <alxl47@naver.com>
2025-03-28 14:23:31 +0900

    패스워드 변경 패스워드 변경 함수 추가

--- miniproject/src/main/java/miniproject/Mapper/Sign_mapper.java
+++ miniproject/src/main/java/miniproject/Mapper/Sign_mapper.java
@@ -2,10 +2,8 @@ package miniproject.Mapper;
 
 import java.util.List;
 
-
-import miniproject.DTO.Md_choiceDTO;
 import miniproject.DTO.Sign_DTO;
-import miniproject.DTO.Web_infoDTO;
+
 
 //회원 관련 mapper
 //함수는 동사로 시작
@@ -15,5 +13,6 @@ public interface Sign_mapper {
 	public int signup_insert(Sign_DTO dto);	//회원 가입 
 	Sign_DTO search_email(String m_name, String m_number); //이메일 찾기
 	public int check_email(String m_email);// 이메일 중복체크
-	public int search_pass(String m_email, String m_number); //패스워드 찾기/변경
+	public String search_pass(String m_email, String m_number); //패스워드 찾기/변경
+	public Integer update_pass(String m_email, String m_pass); //패스워드 변경
 }
