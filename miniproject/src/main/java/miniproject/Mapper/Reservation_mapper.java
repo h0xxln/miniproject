8d4840ae5ad36e1429a2e32529501127440c1cb3
h0xxln <alxl47@naver.com>
2025-04-01 20:56:58 +0900

    방문예약 관련(방문 예약 정보 확인, 방문 예약 신청, 방문예약페이지) Mapper 생성 reservation.do, apply_reservation.do,reservation_check.do

---
+++ miniproject/src/main/java/miniproject/Mapper/Reservation_mapper.java
@@ -0,0 +1,15 @@
+package miniproject.Mapper;
+
+
+import java.util.List;
+import java.util.Map;
+
+import miniproject.DTO.ReservationDTO;
+
+public interface Reservation_mapper {
+		
+		public int apply_reservation(ReservationDTO resdto);
+		public List<ReservationDTO> select_result(Map<Object, Object> tidxAndwv_name);
+		public List<ReservationDTO> check_visit(Map<Object, Object> check_vlist);
+			
+}
