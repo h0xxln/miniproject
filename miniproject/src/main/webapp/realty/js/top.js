233b99fffb532314c8cfbfd5abb51b4075c69518
h0xxln <alxl47@naver.com>
2025-04-03 17:43:12 +0900

    기능 추가 : 회원 정보 아이콘 클릭 시 → 로그인 메뉴 열림/닫힘 마우스를 메뉴 바깥으로 이동 시 → 로그인 메뉴 자동으로 닫힘

--- miniproject/src/main/webapp/realty/js/top.js
+++ miniproject/src/main/webapp/realty/js/top.js
@@ -1,3 +1,21 @@
-function goindex(){
+function goindex() {
 	location.href = "./index.do";
+}
+
+function reserve_page() {
+
+}
+function myinfo_menu(part) {
+	var log_menu = document.getElementById("login_info");
+	if (part == 1) {
+		if (log_menu.style.display == "none") {
+			log_menu.style.display = "block";
+		}
+		else {
+			log_menu.style.display = "none";
+		}
+	}
+	else {
+		log_menu.style.display = "none";
+	}
 }
