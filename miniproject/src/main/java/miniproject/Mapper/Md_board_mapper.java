c6b7da8add542d3731d6d767a01b5d6b9f121376
h0xxln <alxl47@naver.com>
2025-04-03 17:05:47 +0900

    부동산 게시판 Mapper.class 추가
    Md_board_mapper 인터페이스 생성 및 MyBatis 매퍼 등록
    
    게시판 목록 조회, 검색, 상세 조회, 조회수 증가 기능 추가
    
    페이징 및 검색된 게시물 개수 조회 기능 구현

---
+++ miniproject/src/main/java/miniproject/Mapper/Md_board_mapper.java
@@ -0,0 +1,28 @@
+package miniproject.Mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import miniproject.DTO.Md_boardDTO;
+
+@Mapper
+public interface Md_board_mapper {
+	public List<Md_boardDTO> select_mboard(int startIndex, int pageSize);
+	
+	 // 전체 게시물 개수 조회
+    public int getTotalCount();
+	
+    // 조회수 증가
+    public void increaseViews(Long midx);
+    
+    // 게시물 정보 가져오기
+    public List<Md_boardDTO> getPost(Long midx);
+    
+    
+    //검색된 게시물 페이지
+    public List<Md_boardDTO> search_mbpaged(String m_search, Integer startIndex, Integer pageSize); 
+	
+	//검색 게시물 총 갯수
+    public int searchTotal(String m_search);
+}
