6782f500422e33a82d9a450c1bda1566643ac5ed
h0xxln <alxl47@naver.com>
2025-04-21 18:36:09 +0900

    카카오 로그인을 활성화를 위한 dto 필드 추가

--- miniproject/src/main/java/miniproject/DTO/Sign_DTO.java
+++ miniproject/src/main/java/miniproject/DTO/Sign_DTO.java
@@ -19,4 +19,11 @@ public class Sign_DTO {
     String m_privacy;      // 개인정보 수집 및 이용 동의 (ENUM 'Y')
     String m_marketing;    // 마케팅 수신 동의 여부 (ENUM 'Y'/'N')
     String m_date;         // 가입 날짜 (TIMESTAMP) - 기본값 CURRENT_TIMESTAMP
+    
+    
+    //카카오 로그인 
+    String kakao_id;	   // 카카오 아이디
+    String kakao_nicknm;   // 카카오 닉네임
+    String code;		   // 일반회원인지, 카카오회원인지 구분
+    String part; 		   // sign_mapper.xml에서 select시 구분하기 위함(database에 삽입되는 값이 아님)
 }
